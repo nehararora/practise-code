e68b27e98ebaac7e46d6935bf29fedb617c91dc6
Nehar <nehar.arora@gmail.com>
2015-05-11 07:51:50 -0400

    added draw_ruler stub in dsa/python/recursion

--- python/dsa/recursion.py
+++ python/dsa/recursion.py
@@ -20,3 +20,14 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
+
+
+# TODO: draw_ruler
+def draw_english_ruler(scale_length, major_tick_length):
+    """
+    Draws the markings of a typical English ruler.
+    For each inch, places a tick with a numeric label
+    :return:
+    Section 4.1.2, Data Structures and Algorithms in Python, Goodrich et al.
+    """
+    pass
