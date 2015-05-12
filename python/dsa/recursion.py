d63ec8a1834c8704ac6103a55f68ac8d947712f4
Nehar <nehar.arora@gmail.com>
2015-05-12 07:42:02 -0400

    added initial recursive draw ruler + unit test

--- python/dsa/recursion.py
+++ python/dsa/recursion.py
@@ -23,11 +23,41 @@ def factorial(n):
 
 
 # TODO: draw_ruler
-def draw_english_ruler(scale_length, major_tick_length):
-    """
-    Draws the markings of a typical English ruler.
-    For each inch, places a tick with a numeric label
-    :return:
-    Section 4.1.2, Data Structures and Algorithms in Python, Goodrich et al.
-    """
-    pass
+class Ruler(object):
+
+    def __init__(self, inches, major_length, tick_label=''):
+        self.inches = inches
+        self.major_length = major_length
+        self.tick_label = tick_label
+
+    def draw(self):
+        """
+        Draws the markings of a typical English ruler.
+        For each inch, places a tick with a numeric label
+        :return:
+        Section 4.1.2, Data Structures and Algorithms in Python, Goodrich et al.
+        """
+        self.draw_line(self.major_length, '0')  # draw line 0
+        for j in range(1, 1 + self.inches):
+            self.draw_interval(self.major_length - 1)
+            self.draw_line(self.major_length, str(j))  # draw lin j
+
+    @staticmethod
+    def draw_line(tick_length, tick_label=''):
+        line = '-' * tick_length
+        if tick_label:
+            line += ' ' + tick_label
+        print(line)
+
+    def draw_interval(self, center_length):
+        """
+        Draws the interval.
+        :param center_length: Length of the central tick line for this interval.
+        :return:
+        """
+
+        # Draw, top edge, center ticks, and bottom edge
+        if center_length > 0:
+            self.draw_interval(center_length - 1)
+            self.draw_line(center_length)
+            self.draw_interval(center_length - 1)
--- python/dsa/recursion_test.py
+++ python/dsa/recursion_test.py
@@ -5,24 +5,40 @@ recursion_test.py: Test cases for recursion algorithms.
 __author__ = 'nehar'
 
 import unittest
-import recursion
+from recursion import factorial
+from recursion import Ruler
+
 
 
 class TestRecursion(unittest.TestCase):
+
     def test_factorial(self):
         """
 
         :return:
         """
-        self.assertEquals(1, recursion.factorial(0))
-        self.assertEquals(1, recursion.factorial(1))
-        self.assertEquals(2, recursion.factorial(2))
-        self.assertEquals(6, recursion.factorial(3))
-        self.assertEquals(3628800, recursion.factorial(10))
-        self.assertEquals(1307674368000, recursion.factorial(15))
+        self.assertEquals(1, factorial(0))
+        self.assertEquals(1, factorial(1))
+        self.assertEquals(2, factorial(2))
+        self.assertEquals(6, factorial(3))
+        self.assertEquals(3628800, factorial(10))
+        self.assertEquals(1307674368000, factorial(15))
 
         with self.assertRaises(ValueError):
-            recursion.factorial(3/2)
+            factorial(3/2)
         with self.assertRaises(ValueError):
-            recursion.factorial(-4)
+            factorial(-4)
+
+
+    def test_ruler(self):
+        """
+
+        :return:
+        """
+        # TODO: fix - test, instead of printing...
+        Ruler(inches=2, major_length=4).draw()
+        print("\n\n"*5)
+        Ruler(inches=1, major_length=5).draw()
+        print("\n\n"*5)
+        Ruler(inches=3, major_length=3).draw()
 
