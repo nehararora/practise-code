b93720cd4b124d5e33e292ba1fe644d48cd3e86d
Nehar <nehar.arora@gmail.com>
2015-06-04 19:03:52 -0400

    started adding ArrayQueue implementation +tests

--- python/dsa/queues.py
+++ python/dsa/queues.py
@@ -1,8 +1,150 @@
 """
-queues.py: Stack based implementations.
+queues.py: Queue data structure implementations.
 
 refer: Chapter 6, Data Structures and Algorithms in Python, Goodrich et al.
 
 """
 
 __author__ = 'nehar'
+
+
+class ArrayQueue(object):
+    """
+    Array/List based queue.
+
+    Using a plain array will cause O(n) operations on either the dequeue or
+    the enqueue since elements need to get shifted, so we use circular array.
+    """
+
+    ARRAY_SIZE = 5
+
+    def __init__(self):
+        """
+        Initialize queue.
+        """
+        self._array = [None] * ArrayQueue.ARRAY_SIZE
+
+        # track the start and length
+        self._head = 0
+        self._len = 0
+
+    def __len__(self):
+        """
+        Return length of the data structure.
+
+        :return: Number of elements in queue.
+        """
+        return self._len
+
+    def enqueue(self, element):
+        """
+        Append element to end of queue.
+
+        will resize on overflow.
+
+        :param element: element to append.
+        """
+        # resize on overflow
+        if self._len == len(self._array):
+            pass
+        # next open slot is len elements away from front...
+        i = (self._head + self._len) % len(self._array)
+
+        # enqueue and increment count
+        self._array[i] = element
+        self._len += 1
+
+    def dequeue(self):
+        """
+        Remove and return element at front of queue.
+
+        :return: First element in queue.
+        """
+        if self.empty():
+            raise QueueEmptyException()
+        data = self._array[self._head]
+        # move "pointer" to new head
+        self._head = (self._head + 1) % len(self._array)
+        self._len -= 1
+        return data
+
+    def first(self):
+        """
+        Returns the element at front of queue without removing.
+
+        :return: First element in queue.
+        """
+        if self.empty():
+            raise QueueEmptyException()
+        return self._array[self._head]
+
+    def empty(self):
+        """
+        Checks whether queue is empty.
+
+        :return: True if empty, false otherwise.
+        """
+        return True if self._len == 0 else False
+
+    def _resize(self, size):
+        """
+        Resize backing array to specified size.
+
+        :param size: integer value specifying new size.
+        """
+        new_array = [None] * size
+
+        self._array = new_array
+
+
+class DynamicArrayQueue(object):
+    """
+    Custom DynamicArray based Queue implementation.
+    """
+
+    def __init__(self):
+        """
+        Initialize queue.
+        """
+
+    def __len__(self):
+        """
+        Return length of the data structure.
+
+        :return: Number of elements in queue.
+        """
+
+    def enqueue(self, element):
+        """
+        Append element to end of queue.
+
+        :param element: element to append.
+        """
+
+    def dequeue(self):
+        """
+        Remove and return element at front of queue.
+
+        :return: First element in queue.
+        """
+
+    def first(self):
+        """
+        Returns the element at front of queue without removing.
+
+        :return: First element in queue.
+        """
+
+    def empty(self):
+        """
+        Checks whether queue is empty.
+
+        :return: True if empty, false otherwise.
+        """
+
+
+class QueueEmptyException(Exception):
+    """
+    Denote access to empty stack structure.
+    """
+    pass
--- python/dsa/queues_test.py
+++ python/dsa/queues_test.py
@@ -1,1 +1,162 @@
+"""
+queues_tests.py: Test cases for Queue implementations.
+
+"""
+
 __author__ = 'nehar'
+
+
+import unittest
+
+from queues import ArrayQueue
+from queues import DynamicArrayQueue
+from queues import QueueEmptyException
+
+class TestArrayQueue(unittest.TestCase):
+
+    def test_instantiation(self):
+        """
+        Test basic object creation.
+        """
+        s = ArrayQueue()
+        self.assertEqual(0, len(s))
+
+        # backing array defaults to static capacity...
+        self.assertEqual(5, len(s._array))
+
+        # change class internal and try again...
+        ArrayQueue.ARRAY_SIZE = 10
+        s = ArrayQueue()
+        self.assertEqual(0, len(s))
+        self.assertEqual(10, len(s._array))
+
+    def test_enqueue(self):
+        """
+        Test stack push operation.
+        """
+
+        s = ArrayQueue()
+        self.assertEqual(0, len(s))
+
+        # basic push
+        s.push(1)
+        self.assertEqual(1, len(s))
+        self.assertEqual(1, len(s._array))
+        self.assertEqual(1, s._array[0])
+
+        # push an object
+        s.push(QueueEmptyException)
+        self.assertEqual(2, len(s))
+        self.assertEqual(2, len(s._array))
+        self.assertEqual(QueueEmptyException, s._array[1])
+
+    def test_dequeue(self):
+        """
+        Test stack pop operation.
+        """
+
+        s = ArrayQueue()
+        s.push(1)
+        s.push(2)
+        s.push(QueueEmptyException)
+        self.assertEqual(3, len(s))
+        self.assertEqual(QueueEmptyException, s.pop())
+        self.assertEqual(2, len(s))
+        self.assertEqual(2, s.pop())
+        self.assertEqual(1, len(s))
+        self.assertEqual(1, s.pop())
+        self.assertEqual(0, len(s))
+        with self.assertRaises(QueueEmptyException):
+            s.pop()
+
+    def test_first(self):
+        """
+        Test stack peek.
+        """
+        s = ArrayQueue()
+        self.assertEqual(0, len(s))
+
+        with self.assertRaises(QueueEmptyException):
+            s.first()
+
+        # first() always returns object at front of queue...
+        s.enqueue(1)
+        self.assertEqual(1, s.first())
+        s.enqueue([1, 2])
+        self.assertEqual(1, s.first())
+        self.assertEqual(1, s.dequeue())
+        self.assertEqual([1, 2], s.first())
+        self.assertEqual([1, 2], s.dequeue())
+        with self.assertRaises(QueueEmptyException):
+            s.first()
+
+
+class TestDynamicArrayStack(unittest.TestCase):
+
+    def test_instantiation(self):
+        """
+        Test basic object creation.
+        """
+        s = DynamicArrayQueue()
+        self.assertEqual(0, len(s))
+        self.assertEqual(0, len(s._array))
+
+    def test_push(self):
+        """
+        Test stack push operation.
+        """
+
+        s = DynamicArrayQueue()
+        self.assertEqual(0, len(s))
+
+        # basic push
+        s.push(1)
+        self.assertEqual(1, len(s))
+        self.assertEqual(1, len(s._array))
+        self.assertEqual(1, s._array[0])
+
+        # push an object
+        s.push(QueueEmptyException)
+        self.assertEqual(2, len(s))
+        self.assertEqual(2, len(s._array))
+        self.assertEqual(QueueEmptyException, s._array[1])
+
+    def test_pop(self):
+        """
+        Test stack pop operation.
+        """
+
+        s = DynamicArrayQueue()
+        s.push(1)
+        s.push(2)
+        s.push(QueueEmptyException)
+        self.assertEqual(3, len(s))
+        self.assertEqual(QueueEmptyException, s.pop())
+        self.assertEqual(2, len(s))
+        self.assertEqual(2, s.pop())
+        self.assertEqual(1, len(s))
+        self.assertEqual(1, s.pop())
+        self.assertEqual(0, len(s))
+        with self.assertRaises(QueueEmptyException):
+            s.pop()
+
+    def test_peek(self):
+        """
+        Test stack peek.
+        """
+        s = DynamicArrayQueue()
+        self.assertEqual(0, len(s))
+
+        with self.assertRaises(QueueEmptyException):
+            s.peek()
+
+        # try a basic peek
+        s.push(1)
+        self.assertEqual(1, s.peek())
+
+        # try peeking with simple object
+        s.push([1, 2])
+        self.assertEqual([1, 2], s.peek())
+
+
+
