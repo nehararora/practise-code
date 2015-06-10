41d63e8bc18266fc11056734f5facacdc149046b
Nehar <nehar.arora@gmail.com>
2015-06-10 08:39:47 -0400

    started modifying linked lists to use Node based operations (instead of data based)

--- python/dsa/lists.py
+++ python/dsa/lists.py
@@ -1,1 +1,166 @@
+"""
+lists.py: Basic linked list data structure implementations.
+
+refer: Chapter 7, Data Structures and Algorithms in Python, Goodrich et al.
+
+"""
+
 __author__ = 'nehar'
+
+
+class Node(object):
+    __slots__ = "data", "next_node"
+
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    def __eq__(self, other):
+        """
+        Returns True if both object references are same.
+
+        :param other: Node object.
+        :return: boolean status
+        """
+        return self is other
+
+    def __ne__(self, other):
+        """
+        Returns True if object references are not the same.
+
+        :param other: Node object to compare.
+        :return: boolean status
+        """
+        return not self.__eq__(other)
+
+
+class SingleLinkedList(object):
+    """
+    Singly linked list data structure.
+    """
+
+    def __init__(self, head=None):
+        """
+        Initialize list with head node (if passed in.)
+
+        :param head: head node of list.
+        """
+        self._len = 0 if head is None else 1
+        self.head = head
+        self.tail = head
+
+    def first(self):
+        """
+        Return the head node.
+
+        :return: Node object.
+        """
+        return None if self.head is None else self.head
+
+    def last(self):
+        """
+        Return the tail element.
+
+        :return: Node object.
+        """
+        return None if self.tail is None else self.tail
+
+    def before(self, node):
+        """
+        Return the node preceding the specified data.
+
+        :param node: search element.
+        :return: Node object.
+        """
+        # TODO: implement
+
+    def after(self, node):
+        """
+        Return the node succeeding the first occurrence of data.
+
+        :param node: search element.
+        :return: Node object.
+        """
+        # TODO: implement
+
+    def empty(self):
+        """
+        True if list does not contain elements, false otherwise.
+
+        :return: boolean status.
+        """
+        return True if self._len == 0 else False
+
+    def add_first(self, node):
+        """
+        Add Node at head of list.
+
+        :param node: Node object reference.
+        """
+
+        # insert at front of list
+        if self.head is not None:
+            node.next_node = self.head
+        else:
+            # handle first node case
+            self.tail = node
+
+        self.head = node
+        self._len += 1
+
+    def add_last(self):
+        """
+
+        :return:
+        """
+        # TODO: implement
+
+    def add_before(self):
+        """
+
+        :return:
+        """
+        # TODO: implement
+
+    def delete_before(self):
+        """
+
+        :return:
+        """
+        # TODO: implement
+
+    def add_after(self):
+        """
+
+        :return:
+        """
+        # TODO: implement
+
+    def delete_after(self):
+        """
+
+        :return:
+        """
+        # TODO: implement
+
+    def replace(self):
+        """
+
+        :return:
+        """
+        # TODO: implement
+
+    def delete_at(self):
+        """
+
+        :return:
+        """
+        # TODO: implement
+
+    def __len__(self):
+        """
+        Length of list.
+
+        :return: integer count.
+        """
+        return self._len
--- python/dsa/lists_test.py
+++ python/dsa/lists_test.py
@@ -1,1 +1,112 @@
+"""
+lists_test.py:Linked list test module.
+
+"""
+
 __author__ = 'nehar'
+
+import unittest
+from lists import SingleLinkedList
+from lists import Node
+
+
+class TestSingleLinkedList(unittest.TestCase):
+    """
+    Single linked list test class.
+    """
+
+    def test_instantiation(self):
+        """
+        Basic object creation test.
+        """
+        l = SingleLinkedList()
+        self.assertEqual(None, l.head)
+        self.assertEqual(0, l._len)
+        self.assertTrue(l.empty())
+
+        n = Node()
+        self.assertEqual(None, n.data)
+        self.assertEqual(None, n.next_node)
+
+        n = Node(1)
+        self.assertEqual(1, n.data)
+        self.assertEqual(None, n.next_node)
+
+        m = Node(data=2, next_node=n)
+        self.assertEqual(2, m.data)
+        self.assertEqual(n, m.next_node)
+
+    def test_first_last(self):
+        """
+        """
+        l = SingleLinkedList()
+        self.assertIsNone(l.first())
+        self.assertIsNone(l.last())
+
+        n = Node("foo")
+        l.add_first(n)
+        self.assertEqual(n, l.first())
+        self.assertEqual("foo", l.first().data)
+        self.assertEqual(n, l.last())
+        self.assertEqual("foo", l.last().data)
+
+        m = Node("bar")
+        l.add_first(m)
+        self.assertEqual(m, l.first())
+        self.assertEqual("bar", l.first().data)
+        self.assertEqual(n, l.last())
+        self.assertEqual("foo", l.last().data)
+
+    def test_before_after(self):
+        """
+        """
+
+    def test_empty(self):
+        """
+        Test empty status.
+        """
+        l = SingleLinkedList()
+        self.assertTrue(l.empty())
+
+        l.add_first(2)
+        self.assertFalse(l.empty())
+        self.assertEqual(1, len(l))
+        self.assertEqual(1, l._len)
+
+    def test_add_first_last(self):
+        """
+
+        :return:
+        """
+        l = SingleLinkedList()
+
+        l.add_first(node=Node(1))
+        l.add_first(Node(2))
+
+        self.assertEqual(2, len(l))
+        self.assertEqual(2, l.first().data)
+        self.assertEqual(1, l.last().data)
+
+
+    def test_add_before_after(self):
+        """
+
+        :return:
+        """
+    def test_delete_before_after(self):
+        """
+
+        :return:
+        """
+
+    def test_delete_at(self):
+        """
+
+        :return:
+        """
+
+    def test_replace(self):
+        """
+
+        :return:
+        """
