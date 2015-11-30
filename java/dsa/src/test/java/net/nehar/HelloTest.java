9ef3f41684faecee4e517787616e6b9466831c46
Nehar <nehar.arora@gmail.com>
2015-11-29 23:12:03 -0500

    added log4j test resource

--- java/dsa/src/test/java/net/nehar/HelloTest.java
+++ java/dsa/src/test/java/net/nehar/HelloTest.java
@@ -3,20 +3,25 @@ package net.nehar;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
 public class HelloTest{
 
+    private static final Logger logger = LogManager.getLogger(HelloTest.class.getName());
+
     @Before public void setUp(){
 
     }
 
     @Test public void testHelloWorld(){
+        logger.debug("testHelloWorld...");
         Hello hello = new Hello();
-        assertThat("Hello Nehar", is(equalTo(hello.world("Nehar"))));
-
+        assertThat(hello.world("Nehar"), is(equalTo("Hello Nehar")));
+        logger.debug("...done.");
     }
 
     @After public void tearDown(){
