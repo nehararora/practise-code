20d9488f9128852706dd2c2cfb24549619569b48
Nehar <nehar.arora@gmail.com>
2015-12-18 08:10:31 -0500

    added build and run shell scripts

---
+++ java/spring-boot-example/run.fish
@@ -0,0 +1,4 @@
+#! /usr/local/bin/fish
+
+# build and run on fish shell.
+./gradlew build; and  java -jar build/libs/spring-boot-example-0.1.0.jar
---
+++ java/spring-boot-example/run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./gradlew build && java -jar build/libs/spring-boot-example-0.1.0.jar
