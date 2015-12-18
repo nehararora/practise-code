08171ee44d96a2ed1fd831f78b34717bcb845269
Nehar <nehar.arora@gmail.com>
2015-12-18 07:58:43 -0500

    Added log4j2 config to spring boot example

--- java/spring-boot-example/src/main/java/net/nehar/EchoController.java
+++ java/spring-boot-example/src/main/java/net/nehar/EchoController.java
@@ -1,5 +1,7 @@
 package net.nehar;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class EchoController {
 
+    private final static Logger logger = LogManager.getLogger(EchoController.class);
+
     @RequestMapping("/")
     public String index(){
+
+        logger.debug("I shall greets!");
         return "Greets woild!";
     }
 }
