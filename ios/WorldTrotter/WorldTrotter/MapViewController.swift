3e0e13e32e4f9b64fdd33ef16c0e7ce9213f9300
Nehar <nehar.arora@gmail.com>
2016-03-16 07:56:10 -0400

    map view

--- ios/WorldTrotter/WorldTrotter/MapViewController.swift
+++ ios/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -7,12 +7,57 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        // create & set map view
+        mapView = MKMapView()
+        view = mapView
+        
+        // add segmented control
+        let segmentedControl = UISegmentedControl(items: ["Standard",
+                    "hybrid",
+                    "Satellite"])
+        segmentedControl.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self, action: "mapTypeChanged:", forControlEvents: .ValueChanged)
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        // create constraints to match superview.
+        let topConstraint = segmentedControl.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 8)
+        
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor)
+        
+        topConstraint.active = true
+        leadingConstraint.active = true
+        trailingConstraint.active = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("mapController: I loads view")
+    }
+    
+    func mapTypeChanged(segControl: UISegmentedControl){
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .Standard
+        case 1:
+            mapView.mapType = .Hybrid
+        case 2:
+            mapView.mapType = .Satellite
+        default:
+            break
+        }
     }
 
 }
---
+++ ios/WorldTrotter/WorldTrotter/WebViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WebViewController.swift
+//  WorldTrotter
+//
+//  Created by Nehar Arora on 3/15/16.
+//  Copyright © 2016 Nehar Arora. All rights reserved.
+//
+
+import Foundation
