b7e9ced61d9ec62be351d135b785c621b6a44e1c
Nehar <nehar.arora@gmail.com>
2016-03-31 14:52:30 +0530

    minor formatting change

--- ios/Quiz/Quiz/ViewController.swift
+++ ios/Quiz/Quiz/ViewController.swift
@@ -66,7 +66,9 @@ class ViewController: UIViewController {
         self.nextQuestionsLabelCenterXConstraint.constant = 0
         self.currentQuestionLabelCenterXConstraint.constant += screenWidth
 
-        UIView.animateWithDuration(0.5, delay: 0, options: [],
+        UIView.animateWithDuration(0.5,
+                                   delay: 0,
+                                   options: [.CurveEaseIn],
                                    animations: {
                                     self.currentQuestionLabel.alpha = 0
                                     self.nextQuestionLabel.alpha = 1
