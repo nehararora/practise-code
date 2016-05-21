bd87d47fe231bea8b2faa6cccfcb9fc0203f9ab5
Nehar <nehar.arora@gmail.com>
2016-05-21 13:48:07 -0400

    homepwner: added navigation bar titles

--- ios/Homepwner/Homepwner/DetailViewController.swift
+++ ios/Homepwner/Homepwner/DetailViewController.swift
@@ -15,7 +15,11 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var valueField: UITextField!
     @IBOutlet var dateLabel: UILabel!
 
-    var item: Item!
+    var item: Item! {
+        didSet {
+            navigationItem.title = item.name
+        }
+    }
 
     let numberFormatter: NSNumberFormatter = {
        let formatter = NSNumberFormatter()
