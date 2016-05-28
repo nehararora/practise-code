81e830d463dafa97d10bb8699a7626e43db8b19b
Nehar <nehar.arora@gmail.com>
2016-05-28 12:49:55 -0400

    homepwner: added saving images

--- ios/Homepwner/Homepwner/ImageStore.swift
+++ ios/Homepwner/Homepwner/ImageStore.swift
@@ -11,15 +11,52 @@ import UIKit
 class ImageStore {
     let cache = NSCache()
 
+    // MARK: - CRUD operations
     func setImage(image: UIImage, forKey key:String) {
         cache.setObject(image, forKey: key)
+
+        // create url for image
+        let imageURL = imageURLForKey(key)
+        if let data = UIImageJPEGRepresentation(image, 0.5) {
+            data.writeToURL(imageURL, atomically: true)
+        }
     }
 
     func imageForKey(key: String) -> UIImage? {
-        return cache.objectForKey(key) as? UIImage
+        // check for cached image first
+        if let existingImage = cache.objectForKey(key) as? UIImage {
+            return existingImage
+        }
+
+        let imageURL = imageURLForKey(key)
+
+        guard let imagefromDisk = UIImage(contentsOfFile: imageURL.path!) else {
+            return nil
+        }
+        cache.setObject(imagefromDisk, forKey: key)
+        return imagefromDisk
+
     }
 
     func deleteForKey(key:String) {
+        // remove from cache...
         cache.removeObjectForKey(key)
+
+        // and disk
+        let imageURL = self.imageURLForKey(key)
+        do {
+            try NSFileManager.defaultManager().removeItemAtURL(imageURL)
+        } catch let deleteError {
+            print("Error removing image from disk \(deleteError)")
+        }
+
+
+    }
+
+    // MARK: - Utility methods
+    func imageURLForKey(key: String) -> NSURL {
+        let documentDirectories = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        let documentDirectory = documentDirectories.first!
+        return documentDirectory.URLByAppendingPathComponent(key)
     }
 }
